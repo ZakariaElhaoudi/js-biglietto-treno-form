4bdbd19d9c51c75944813c84c7bb9a883ad62b25
Zakaria96 <zakarialavoro@gmail.com>
2023-04-28 18:14:57 +0200

    parte logica esercizio completato

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -5,51 +5,49 @@ const costoKm = 0.21 ;
 const scontoUnder18 = 20;
 const scontoOver65 = 40;
 
-// chiedere all’utente il numero di chilometri che vuole percorrere
-const kmPercorso =document.getElementById("km").value;
-console.log(kmPercorso);
 
-// chiedere all’utente l’età del passeggero
-const ageUser = document.getElementById("eta").value;
-console.log(ageUser);
-
-// calcolare il prezzo totale del viaggio
-let costoViaggio = costoKm * kmPercorso;
-console.log(costoViaggio);
-
-// applicare uno sconto al biglietto del 40% per gli over 65 e del 20% per i minorenni
-let scontoViaggio; 
-
-if (ageUser > parseInt(65)) { // applicare uno sconto del 40% per gli over 65
-    scontoViaggio = costoViaggio - (costoViaggio * scontoOver65/ 100);
-} else if (ageUser < parseInt(18)) { // applicare uno sconto del 20% per i minorenni
-    scontoViaggio = costoViaggio - (costoViaggio * scontoUnder18/ 100);
-} else {
-    scontoViaggio = costoViaggio 
-}
-console.log(scontoViaggio);
-
-let costoTotale = scontoViaggio.toFixed(2);
-
-// L’output del prezzo finale 
-document.getElementById("prezzo-biglietto").innerHTML = "Costo biglietto € "+ costoTotale;
-
-// L’output del prezzo finale tramite click button
-// function myFunction() {
-//     document.getElementById("prova").innerHTML = costoTotale;
-//   }
-
-
-document.getElementById("prova").addEventListener("click", myFunction);
-
+// L’output del prezzo finale tramite click 
 function myFunction() {
-  document.getElementById("prova").innerHTML = costoTotale;
+    
+    // chiedere all’utente il numero di chilometri che vuole percorrere
+    const kmPercorso = document.getElementById("km").value;
+    console.log(kmPercorso);
+
+    // chiedere all’utente l’età del passeggero
+    const ageUser = document.getElementById("eta").value;
+    console.log(ageUser);
+
+    // calcolare il prezzo totale del viaggio
+    let costoViaggio = costoKm * kmPercorso;
+    console.log(costoViaggio);
+
+    // applicare uno sconto al biglietto del 40% per gli over 65 e del 20% per i minorenni
+    let scontoViaggio; 
+
+    if (ageUser > parseInt(65)) { // applicare uno sconto del 40% per gli over 65
+    scontoViaggio = costoViaggio - (costoViaggio * scontoOver65/ 100);
+    } else if (ageUser < parseInt(18)) { // applicare uno sconto del 20% per i minorenni
+    scontoViaggio = costoViaggio - (costoViaggio * scontoUnder18/ 100);
+    } else {
+    scontoViaggio = costoViaggio 
+    }
+    console.log(scontoViaggio);
+
+    let costoTotale = scontoViaggio.toFixed(2);
+
+    // L’output del prezzo finale 
+    const risultatoDiv = document.getElementById("risultato");
+    risultatoDiv.style.display = "block"; // mostra il risultato
+    document.getElementById("prezzo-biglietto").innerHTML = costoTotale;
 }
-
-document.getElementById("prova").addEventListener("click", myFunction);
-
+    // reset dei dati inseriti 
 function myFunction2() {
     document.getElementById("myForm").reset();
 }
+
+function resetResult() {
+        document.getElementById("risultato").style.display = "none";
+}
+
 
 
